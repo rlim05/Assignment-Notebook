e8b4160fb5cd77070f136ca01882b83bb4f45781
Ryan Lim <rlim@bsd220.org>
2022-01-13 20:16:20 -0600

    Added .onMove .onDelete and Edit Buttons

--- Assignment Notebook/ContentView.swift
+++ Assignment Notebook/ContentView.swift
@@ -10,12 +10,26 @@ import SwiftUI
 struct ContentView: View {
     @State private var assignmentItems =
         [AssignmentItem(course: "Pre Calc", description: "", dueDate: Date()),
-         AssignmentItem(course: "Physics", description: "", dueDate: Date()),
-         AssignmentItem(course: "Englsh RA", description: "", dueDate: Date()),
-         AssignmentItem(course: "Mobile Apps Development", description: "", dueDate: Date())]
+         AssignmentItem(course: "Mobile Apps Development", description: "", dueDate: Date()),
+         AssignmentItem(course: "U.S. History", description: "", dueDate: Date()),
+         AssignmentItem(course: "English RA", description: "", dueDate: Date()),
+         AssignmentItem(course: "Spanish", description: "", dueDate: Date()),
+         AssignmentItem(course: "Physics", description: "", dueDate: Date())]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach(assignmentItems) { item in
+                    Text(item.description)
+                }
+                .onMove(perform: {indices, newOffset in assignmentItems.move(fromOffsets: indices, toOffset: newOffset)
+                })
+                .onDelete(perform: { indexSet in
+                    assignmentItems.remove(atOffsets: indexSet)
+                })
+            }
+            .navigationBarTitle("Assignment Notebook", displayMode: .inline)
+            .navigationBarItems(leading: EditButton())
+        }
     }
 }
 
