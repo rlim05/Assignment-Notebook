e26350ae22d38240a9e05f0193869baab5679dd9
Ryan Lim <rlim@bsd220.org>
2022-01-13 20:42:24 -0600

    Created AddAssignmentView

---
+++ Assignment Notebook/AddAssignmentView.swift
@@ -0,0 +1,34 @@
+//
+//  AddAssignmentView.swift
+//  Assignment Notebook
+//
+//  Created by Student on 1/13/22.
+//
+
+import SwiftUI
+
+struct AddAssignmentView: View {
+    @ObservedObject var assignmentList: AssignmentList
+    @State private var course = ""
+    @State private var description = ""
+    @State private var dueDate = Date()
+    @Environment(\.presentationMode) var presentationMode
+    static let courses = ["Pre Calc", "Mobile Apps Development", "U.S. History", "English RA", "Spanish", "Physics"]
+    var body: some View {
+        NavigationView {
+            Form {
+                Picker("Course", selection: $course) {
+                    ForEach(Self.courses, id: \.self) { course in
+                        Text(course)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct AddAssignmentView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddAssignmentView(assignmentList: AssignmentList())
+    }
+}
